c4690fd449a31d9f937471e6a6c848038f821ec9
hermes4269 <hermes_4269@yahoo.com>
2017-06-21 13:43:18 +0200

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -34,6 +34,8 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+		moja1();
+		 
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
@@ -47,3 +49,28 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
+
+	function moja1(){
+		alert("mojaaa");
+		
+	}
+   var onSuccess = function(position) {
+        alert('Latitude: '          + position.coords.latitude          + '\n' +
+              'Longitude: '         + position.coords.longitude         + '\n' +
+              'Altitude: '          + position.coords.altitude          + '\n' +
+              'Accuracy: '          + position.coords.accuracy          + '\n' +
+              'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+              'Heading: '           + position.coords.heading           + '\n' +
+              'Speed: '             + position.coords.speed             + '\n' +
+              'Timestamp: '         + position.timestamp                + '\n');
+    };
+
+    // onError Callback receives a PositionError object
+    //
+    function onError(error) {
+        alert('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
+    }
+
+ 
+  navigator.geolocation.getCurrentPosition(onSuccess, onError);
